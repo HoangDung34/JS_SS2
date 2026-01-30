101eed3ecd01e53561638a986ee601fc5ae62235
HoangDung34 <hngdungtr55@gmail.com>
2026-01-30 13:28:39 +0700

    Update BTVN06.js

--- HoangDinhTrungDung_B25DTCN236_IT103B_SESSION02/Bài Tập/BTVN06.js
+++ HoangDinhTrungDung_B25DTCN236_IT103B_SESSION02/Bài Tập/BTVN06.js
@@ -4,18 +4,20 @@ let status = prompt("Nhập tình trạng sách (có sẵn/đã mượn/không c
 let borrowDays = Number(prompt("Nhập số ngày mượn:"));
 let card = prompt("Bạn có thẻ thư viện không? (Có/Không):");
 
-if (status == "có sẵn" && card == "Có") {
+if (status === "có sẵn" && card === "Có") {
     console.log("Chúc mừng, bạn có thể mượn sách này");
-} else if (bookStatus == "đã mượn" && borrowDays < 30) {
-    if (card == "Có") {
+} else if (status === "đã mượn" && borrowDays < 30) {
+    if (card === "Có") {
         console.log("Sách đang được mượn, vui lòng đợi đến khi trả lại");
     } else {
         console.log("Bạn không thể mượn sách nếu không có thẻ thư viện");
     }
-} else if (bookStatus == "Không có sẵn") {
+} else if (status === "Không có sẵn") {
     console.log("Sách này hiện tại không có sẵn trong thư viện, bạn có thể đăng ký mượn sau");
 } else {
     console.log("Thông tin không hợp lệ, vui lòng nhập lại");
 }
+
+
 
 
